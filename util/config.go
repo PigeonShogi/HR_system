e1cac3cc9d77ef70a80b4bb242370508a1712705
Ckai <ckai.trans@gmail.com>
2023-05-19 15:54:19 +0800

    add app.env for Viper integration

---
+++ util/config.go
@@ -0,0 +1,26 @@
+package util
+
+import "github.com/spf13/viper"
+
+// 利用 Config 儲存應用程式的所有設定，其值來自設定檔或環境變數，並以 viper 讀取。
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
